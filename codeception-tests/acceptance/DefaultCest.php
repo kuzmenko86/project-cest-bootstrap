a14fe13175bf7ad2fe99b3c63b65c7b257408715
kuzmenko86 <kuzmenko86@gmail.com>
2013-11-21 15:35:54 +0200

    No issue, commit changes

--- codeception-tests/acceptance/DefaultCest.php
+++ codeception-tests/acceptance/DefaultCest.php
@@ -17,58 +17,19 @@ class DefaultCest
     {
     }
 
-    // tests
-    public function tryToTest35699(WebGuy $I) {
 
+
+    public function tryToSpendCheckout(WebGuy $I){
         \ISM\PageFactory::setGuy($I);
         $I->wantTo('test home page');
         $page = \ISM\PageFactory::getPage('home');
-        $page->amOnPage();
-        $page->checkBannerRotator();
+        $page->amOnPage('/electronics/computers/build-your-own/gaming-computer.html');
+        $I->click('Customize and Add to Cart');
+        $I->wait(3000);
+        $I->selectOption("//input[@id='bundle-option*']","54");
+        $I->wait(3000);
+        //$page->checkBannerRotator();
 
-    }
-
-    private function tryToMakeOrder(WebGuy $I){
-        $I->wantTo('make a purchase on Defoult magento environment');
-
-        $I->amOnPage("/apparel/classic-jeans.html");
-
-        $I->click('Add to Cart');
-        $I->click('Proceed to Checkout');
-        $I->checkOption('login:guest');
-        $I->click('Continue');
-
-        $I->click("//div[@id='billing-buttons-container']/button");//continue button on checkout
-        $I->see('This is a required field.');
-        $I->fillField("//input[@id='billing:firstname']","Volodymyr");
-        $I->fillField("//input[@id='billing:lastname']","Kuzmenko");
-        $I->fillField("//input[@id='billing:email']","v.kuzmenko@ism-ukraine.com");
-        $I->fillField("//input[@id='billing:street1']","my test address");
-        $I->fillField("//input[@id='billing:city']","Zhitomir");
-        $I->fillField("//input[@id='billing:postcode']","1911JA");
-        $I->fillField("//input[@id='billing:telephone']","0855555555");
-        $I->click("//div[@id='billing-buttons-container']/button");//continue button on checkout
-
-        $I->checkOption('billing:use_for_shipping_yes');
-        sleep(1);
-        $I->click("//div[@id='shipping-buttons-container']/button");//continue button on checkout
-        //$I->click(".//*[@id='shipping-buttons-container']/button");//continue button on checkout
-
-        sleep(1);
-        $I->click("//div[@id='shipping-method-buttons-container']/button");//continue button on checkout
-        //$I->checkOption('s_method_freeshipping_freeshipping');
-
-        sleep(1);
-        $I->checkOption('p_method_checkmo');
-        $I->click("//div[@id='payment-buttons-container']/button");//continue button on checkout
-
-        sleep(1);
-        $I->click('Place Order');
-
-
-        sleep(10);
-
-        //$I->click('');
 
     }
 
