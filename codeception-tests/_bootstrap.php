29ced1e2b3fb774e143e003707ed2cfb06d7e885
Alexander Velikdus <a.velikdus@ism-ukraine.com>
2013-11-29 13:44:10 +0200

    NO ISSUE: Remove application source from local to src.

--- codeception-tests/_bootstrap.php
+++ codeception-tests/_bootstrap.php
@@ -2,7 +2,8 @@
 // This is global bootstrap for autoloading
 define ('DS', DIRECTORY_SEPARATOR);
 define ('ISM_NS', 'ISM');
-$localAppDirPrefix = __DIR__. DS. 'local'. DS . 'app' . DS . ISM_NS;
+$localAppDirPrefix = __DIR__. DS. '..' . DS .'src'. DS . 'app' . DS . ISM_NS;
+$localAppDirPrefix = realpath($localAppDirPrefix);
 Codeception\Util\Autoload::register(ISM_NS, 'Page', $localAppDirPrefix);
 Codeception\Util\Autoload::register(ISM_NS, 'Factory', $localAppDirPrefix.DS.'Factory');
 Codeception\Util\Autoload::register(ISM_NS.'\\Intface', 'Intface', $localAppDirPrefix.DS.'Intface');
