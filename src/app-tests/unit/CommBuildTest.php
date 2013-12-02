14fc5aa3a68aeb61be35a3b61c94ff7acac1738f
Alexander Velikdus <a.velikdus@ism-ukraine.com>
2013-12-02 13:28:11 +0200

    #12 Improve build script with using classes. Add test set target test.

--- src/app-tests/unit/CommBuildTest.php
+++ src/app-tests/unit/CommBuildTest.php
@@ -3,6 +3,9 @@ use Codeception\Util\Stub;
 
 class CommBuildToolTest extends \Codeception\TestCase\Test
 {
+    private $_srcPath;
+    private $_trgPath;
+
    /**
     * @var \CodeGuy
     */
@@ -10,6 +13,8 @@ class CommBuildToolTest extends \Codeception\TestCase\Test
 
     protected function _before()
     {
+        $this->_srcPath = __DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . 'codeception-tests' . DS . 'acceptance' . DS . 'WebGuy.php';
+        $this->_trgPath = __DIR__ . DS . ".."  . DS . "..". DS . 'app' . DS . 'ISM' . DS . "AbstractPage.php";
     }
 
     protected function _after()
@@ -17,34 +22,44 @@ class CommBuildToolTest extends \Codeception\TestCase\Test
     }
 
     // tests
-    public function testExecutionWithOpenSrc()
+    public function testExecutionWithOpenSrcTrg()
     {
         $c = new \ISM\Tool\CommBuildTool;
-        $srcOld = __DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . 'codeception-tests' . DS . 'acceptance' . DS . 'WebGuy.php';
-        $src = realpath($srcOld);
+        $src = realpath($this->_srcPath);
         // check does file exist
-        $this->assertTrue( !  empty($src),  'File not found =>' . $srcOld);
-
+        $this->assertTrue( !  empty($src),  'Src file not found => ' . $this->_srcPath);
         $c->setSource($src);
         $result = $c->openSrcFile();
         $this->assertTrue($result, 'CommBuildTool open src file failed');
+
+        $trg = realpath($this->_trgPath);
+        // check does file exist
+        $this->assertTrue( ! empty($trg),  'Trg file not found => ' . $this->_trgPath);
+
+        $c->setTarget($trg);
+        $result = $c->openTrgFile();
+
+        $this->assertTrue($result, 'CommBuildTool open trg file failed');
 
         $result = $c->execute();
         $this->assertTrue($result, 'CommBuildTool failed');
     }
 
-    // tests
-    public function testExecutionWithSetResult()
+    public function testExecutionWithSetSourceResult()
     {
         $c = new \ISM\Tool\CommBuildTool;
-        $srcOld = __DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . 'codeception-tests' . DS . 'acceptance' . DS . 'WebGuy.php';
-        $src = realpath($srcOld);
+        $src = realpath($this->_srcPath);
         // check does file exist
-        $this->assertTrue( !  empty($src),  'File not found =>' . $srcOld);
-
+        $this->assertTrue( !  empty($src),  'File not found =>' . $this->_srcPath);
         $c->setSource($src);
+
+        $trg = realpath($this->_trgPath);
+        // check does file exist
+        $this->assertTrue( ! empty($trg),  'Trg file not found => ' . $this->_trgPath);
+        $c->setTarget($trg);
+
         $result = $c->execute();
-        $this->assertTrue($result, 'CommBuildTool failed');
+        $this->assertTrue($result, 'CommBuildTool method execution() failed');
     }
 
 }
--- src/app/ISM/Tool/CommBuildTool.php
+++ src/app/ISM/Tool/CommBuildTool.php
@@ -9,16 +9,20 @@ class CommBuildTool implements CommandIntface
     protected $_src = null;
     protected $_trg = null;
     protected $_srcHandle = null;
-
+    protected $_trgHandle = null;
 
     public function execute()
     {
         $result = true;
-        if (is_null($this->_src)) {
+        if (is_null($this->_src) || is_null($this->_trg)) {
             return false;
         }
         if (is_null($this->_srcHandle)) {
             $result = $this->openSrcFile();
+        }
+
+        if (is_null($this->_trgHandle)) {
+            $result = $this->openTrgFile();
         }
 
         return $result;
@@ -29,6 +33,11 @@ class CommBuildTool implements CommandIntface
         $this->_src = $src;
     }
 
+    public function setTarget($trg)
+    {
+        $this->_trg = $trg;
+    }
+
     public function openSrcFile()
     {
         if ($this->_srcHandle) {
@@ -37,6 +46,21 @@ class CommBuildTool implements CommandIntface
         $handle = @fopen($this->_src, "r");
         if ($handle) {
             $this->_srcHandle = $handle;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function openTrgFile($mode = 'r')
+    {
+        if ($this->_trgHandle) {
+            return true;
+        }
+        $handle = @fopen($this->_trg, $mode);
+        if ($handle) {
+            $this->_trgHandle= $handle;
             return true;
         } else {
             return false;
