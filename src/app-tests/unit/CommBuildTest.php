08c519cccaca405644f9e1e49529890f530963e9
Alexander Velikdus <a.velikdus@ism-ukraine.com>
2013-12-02 13:00:45 +0200

    #12 Improve build script with using classes. Test execution.

--- src/app-tests/unit/CommBuildTest.php
+++ src/app-tests/unit/CommBuildTest.php
@@ -17,9 +17,32 @@ class CommBuildToolTest extends \Codeception\TestCase\Test
     }
 
     // tests
-    public function testExecutionResult()
+    public function testExecutionWithOpenSrc()
     {
         $c = new \ISM\Tool\CommBuildTool;
+        $srcOld = __DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . 'codeception-tests' . DS . 'acceptance' . DS . 'WebGuy.php';
+        $src = realpath($srcOld);
+        // check does file exist
+        $this->assertTrue( !  empty($src),  'File not found =>' . $srcOld);
+
+        $c->setSource($src);
+        $result = $c->openSrcFile();
+        $this->assertTrue($result, 'CommBuildTool open src file failed');
+
+        $result = $c->execute();
+        $this->assertTrue($result, 'CommBuildTool failed');
+    }
+
+    // tests
+    public function testExecutionWithSetResult()
+    {
+        $c = new \ISM\Tool\CommBuildTool;
+        $srcOld = __DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . 'codeception-tests' . DS . 'acceptance' . DS . 'WebGuy.php';
+        $src = realpath($srcOld);
+        // check does file exist
+        $this->assertTrue( !  empty($src),  'File not found =>' . $srcOld);
+
+        $c->setSource($src);
         $result = $c->execute();
         $this->assertTrue($result, 'CommBuildTool failed');
     }
--- src/app/ISM/Tool/CommBuildTool.php
+++ src/app/ISM/Tool/CommBuildTool.php
@@ -6,8 +6,41 @@ use \ISM\Intface\CommandIntface;
 
 class CommBuildTool implements CommandIntface
 {
+    protected $_src = null;
+    protected $_trg = null;
+    protected $_srcHandle = null;
+
+
     public function execute()
     {
-        return true;
+        $result = true;
+        if (is_null($this->_src)) {
+            return false;
+        }
+        if (is_null($this->_srcHandle)) {
+            $result = $this->openSrcFile();
+        }
+
+        return $result;
+    }
+
+    public function setSource($src)
+    {
+        $this->_src = $src;
+    }
+
+    public function openSrcFile()
+    {
+        if ($this->_srcHandle) {
+            return true;
+        }
+        $handle = @fopen($this->_src, "r");
+        if ($handle) {
+            $this->_srcHandle = $handle;
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
